7de35b0b80da87c0b2355caa5a7af3a4434c9f52
Nov05 <arwen.liuwenjing@gmail.com>
2019-04-23 16:20:13 -0500

    finished the app

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Shiny-Text_Input_Prediction_V2
+# Shiny-Text_Input_Prediction-V2
 This is an R Shiny app.
+
+## How to run the app?
+
+1. Visit the app online with this URL: https://wenjing.shinyapps.io/app-text-input-v2/
+2. Or launch the app by running the following code in your RStudio.
+
+```
+library(shiny); runGitHub( "Shiny-Text_Input_Prediction_V2", "Nov05")
+```
+
+
+<br><br>
+For more information:   
+Shiny - https://shiny.rstudio.com/tutorial/written-tutorial/lesson7/ 
--- app.R
+++ app.R
@@ -1,10 +1,4 @@
 
-# https://stackoverflow.com/questions/35265920/auto-complete-and-selection-of-multiple-values-in-text-box-shiny
-
-# CAUTION: Full 1-gram dataset contains too many objects,
-#          which makes it impossible for the app to load.
-# unigram <- readRDS("1gram_en.rds")
-
 library(shiny)
 library(shinysky)
 library(ggplot2)
@@ -14,82 +8,84 @@ library(stringr)
 # remove all objects
 rm(list = ls())
 
-unigram <- readRDS("1gram_90_en.rds")
-unilist <- as.character((unigram[,1])); rm(unigram)
 trigram <- readRDS("3gram_notail_en.rds")
-text <- ""
-text_all <- list()
+words <- list()
 
 ui <- shinyUI(
-  fluidPage(tags$style(type="text/css",".shiny-output-error { visibility: hidden; }",".shiny-output-error:before { visibility: hidden; }"),
-            tags$style(type="text/css","#search { top: 50% !important;left: 50% !important;margin-top: -100px !important;margin-left: -250px 
-                       !important; color: blue;font-size: 20px;font-style: italic;}"),         
-            
-            titlePanel("Text Input Prediction"),
+  fluidPage(titlePanel("Text Input Prediction"),
       
-            sidebarLayout(
-              sidebarPanel(
-                helpText("Please type one word a time in the input box below."),
-                textInput.typeahead(id="typeahead01",
-                                    placeholder="Type a word here",
-                                    local=data.frame(name=c(unilist)),
-                                    valueKey="name",
-                                    tokens=c(1:length(unilist)),
-                                    template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>"),
-                                    limit=10
-                ), # end of textInput.typeahead()
-                shinyalert("shinyalert01"),
-                textInput("textInput01", label="Input History", value = "", width = NULL,
-                          placeholder='Display words here'),
-                tableOutput('tableOutput01'),
-                actionButton("actionButton01", "Clear Input")
-              ),
-              mainPanel(
-                plotOutput("barplot01")
-              )
-            ) # end of sidebarLayout()
+    sidebarLayout(
+      sidebarPanel(
+        
+        tags$b("Please type some words in the box below."),
+        textInput("textInput01", label=NULL, 
+                  value = "", width=NULL,
+                  placeholder=''),
+        shinyalert("shinyalert01"),
+        tags$b("The most possible input predicted:"),
+        verbatimTextOutput("verbatimTextOutput01", placeholder=TRUE),
+        tableOutput('tableOutput01'),
+        actionButton("actionButton01", "Clear Input")
+      ),
+      
+      mainPanel(
+        plotOutput("barplot01")
+      )
+    ) # end of sidebarLayout()
   ) # end of fluidPage()
 )
 
 server <- function(input, output, session){
   # typeahead
   observe({
-    input$typeahead01 # read-only
+    input$textInput01 # read-only
+
+    # alphabetial and lower-case only 
+    text <- tolower(input$textInput01)
+    text <- gsub("^[^a-z]+|[^a-z]+$", " ", text)
+    text <- str_trim(gsub("\\s+", " ", text))
     
-    text <- str_trim(input$typeahead01)
-    if (text != ""){
-      # append input to list
-      text_all[length(text_all)+1] <<- str_trim(gsub("\\s+", " ", text))
-      
+    # convert input to a list of words
+    words <<- unlist(strsplit(text, " "))
+    
+    if (length(words) == 0){
       # show message
-      showshinyalert(session=session, 
-                     id="shinyalert01", 
-                     HTMLtext=paste0("You just inputted \"", text, "\"."), 
-                     styleclass="success")
-      
-      # search 3-gram dictionary
-      # match with two words
-      pred <- head(trigram[grep(paste0("^", str_trim(paste0(text_all[length(text_all)-1], " ",
-                                                            text_all[length(text_all)])), " "), 
-                                trigram[,'ngrams']),], 20)
-      # match with the last word
-      if (length(text_all)>1 & dim(pred)[1]==0){
-        pred <- head(trigram[grep(paste0("^", text_all[length(text_all)], " "), 
-                                  trigram[,'ngrams']),], 20)}
-      # update text input
-      updateTextInput.typeahead(session, "typeahead01",
-                                placeholder="Type a word here",
-                                dataset=data.frame(name=c(unilist)),
-                                valueKey="name",
-                                tokens=c(1:length(unilist)),
-                                template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>"),
-                                limit=10)
-      # update input history
-      updateTextInput(session, "textInput01", value=paste(text_all, collapse=" "))
-      
+      if (str_trim(input$textInput01) != ""){
+        showshinyalert(session=session, 
+                       id="shinyalert01", 
+                       HTMLtext=paste0("\"", input$textInput01, 
+                                       "\" isn't valid input for prediction."), 
+                       styleclass="danger")
+      }
+      output$verbatimTextOutput01 <- renderText(NULL)
+      output$tableOutput01 <- renderTable(NULL)
+      output$barplot01 <- renderPlot(NULL)
+      return()
+    } 
+
+    # show message
+    showshinyalert(session=session, 
+                   id="shinyalert01", 
+                   HTMLtext=paste0("Words for prediction are \"", text, "\"."), 
+                   styleclass="info")
+    
+    # search 3-gram dictionary
+    # match with the last two words
+    pred <- head(trigram[grep(paste0("^", str_trim(paste0(words[length(words)-1], " ",
+                                                          words[length(words)])), " "), 
+                              trigram[,'ngrams']),], 20)
+    # match with the last word
+    if (length(words)>1 & dim(pred)[1]==0){
+      pred <- head(trigram[grep(paste0("^", words[length(words)], " "), 
+                                trigram[,'ngrams']),], 20)}
+    
+    if (dim(pred)[1]==0){pred <- trigram[1:20,]}
+    
+    if (dim(pred)[1] != 0){
+      # output the prediction
+      output$verbatimTextOutput01 <- renderText({pred[1,1]})
       # show predicted words table
-      output$tableOutput01 <- renderTable(pred[1:5,])
-      
+      output$tableOutput01 <- renderTable(head(pred))
       # plot bar chart
       output$barplot01 <- renderPlot({
         p01 <- ggplot(data=pred, aes(x=reorder(ngrams, -prob), y=prob)) +
@@ -101,34 +97,29 @@ server <- function(input, output, session){
                      axis.text.x=element_blank(),
                      axis.text.y=element_text(angle=90))
         print(p01)
-      }) # end of renderPlot()    
-    }
-  }) # end of observe input$typeahead01
-  
-  observeEvent(input$actionButton01, {
-    # clear variants
-    text <<- ""
-    text_all <<- list()
+      }) # end of renderPlot()  
+      return()
+    } # if (dim(pred)[1] != 0)
     
-    # update text input
-    updateTextInput.typeahead(session, "typeahead01",
-                              placeholder="Type a word here",
-                              dataset=data.frame(name=c(unilist)),
-                              valueKey="name",
-                              tokens=c(1:length(unilist)),
-                              template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p>"),
-                              limit=10)
-    
-    # update input history
-    updateTextInput(session, "textInput01", value="")
+    output$verbatimTextOutput01 <- renderText("(No prediction)")
     output$tableOutput01 <- renderTable(NULL)
     output$barplot01 <- renderPlot(NULL)
     
+  }) # end of observe input$typeahead01
+  
+  # click on "Clear Input" button
+  observeEvent(input$actionButton01, {
+    words <<- list()
+    updateTextInput(session, "textInput01", value="")
+    output$verbatimTextOutput01 <- renderText(NULL)
+    output$tableOutput01 <- renderTable(NULL)
+    output$barplot01 <- renderPlot(NULL)
     # show message
     showshinyalert(session=session, 
                    id="shinyalert01", 
-                   HTMLtext=paste0("All inputs have been cleared."), 
+                   HTMLtext=paste0("Input has been cleared."), 
                    styleclass="success")
+    return()
   }) # end of observeEvent input$actionButton01
   
 } # end of server function
